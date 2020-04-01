af6047b8a15f20f079ae951234ec97cad1fbdb46
cbxa <895001956@qq.com>
2020-04-01 15:06:39 +0800

    完成了表单element-ui

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,132 @@
 <template >
  <div id="login">
-  欢迎来到的登录页面
-  <el-button type="success">成功按钮</el-button>
+  <div class="login-wrap">
+   <ul class="menu-tab">
+    <li v-for="item in mentTab" :key="item.id" :class="{'current':item.current}" @click="toggleMenu(item)">{{item.txt}}</li>
+   </ul>
+   <!--表单开始-->
+   <el-form  :model="ruleForm" status-icon :rules="rules" ref="ruleForm"  class="login-form" size="medium">
+    <el-form-item prop="username" class="item-form">
+     <label>用户名</label>
+     <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item  prop="password"  class="item-form">
+     <label>密码</label>
+     <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="9"></el-input>
+    </el-form-item>
+    <el-form-item  prop="code"  class="item-form">
+     <label>验证码</label>
+     <el-row :gutter="20">
+      <el-col :span="24">
+       <el-input v-model.number="ruleForm.code" minlength="6" maxlength="6"></el-input>
+      <el-col :span="6">
+      <el-button type="success" class="center">获取验证码</el-button></el-col>
+     </el-col>
+
+     </el-row>
+
+    </el-form-item>
+    <el-form-item>
+     <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+     <el-button @click="resetForm('ruleForm')">重置</el-button>
+    </el-form-item>
+   </el-form>
+  </div>
  </div>
 </template>
-
 <script>
     export default {
-        name: "login"
+        name: "login",
+        data(){
+            // var checkAge = (rule, value, callback) => {
+            //     if (!value) {
+            //         return callback(new Error('年龄不能为空'));
+            //     }
+            //     setTimeout(() => {
+            //         if (!Number.isInteger(value)) {
+            //             callback(new Error('请输入数字值'));
+            //         } else {
+            //             if (value < 18) {
+            //                 callback(new Error('必须年满18岁'));
+            //             } else {
+            //                 callback();
+            //             }
+            //         }
+            //     }, 1000);
+            // };
+            //验证用户名
+            var validateUsername = (rule, value, callback) => {
+                let reg=/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+                if (value === '') {
+                    callback(new Error('请输入用户名'));
+                } else if(!reg.test(value)){
+                    callback(new Error('用户名格式有误'));
+                }else {
+                    callback();//true
+
+                }
+            };
+            //验证密码
+            var validatePassword = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入密码'));
+                } else {
+                    callback();
+                }
+            };
+            //验证验证码
+            var validateCode = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入验证码'));
+                } else {
+                    callback();
+                }
+            };
+            return{
+                mentTab:[
+                    {txt:'登录',current:true},
+                    {txt:'注册',current:false}
+                ],
+                ruleForm: {
+                    username: '',
+                    password: '',
+                    code: ''
+                },
+                rules: {
+                    username: [
+                        { validator: validateUsername, trigger: 'blur' }
+                    ],
+                    password: [
+                        { validator: validatePassword, trigger: 'blur' }
+                    ],
+                    code: [
+                        { validator: validateCode, trigger: 'blur' }
+                    ]
+                }
+            }
+        },
+        methods:{
+            toggleMenu(data){
+                // console.log(data)
+                this.mentTab.forEach(item=>{
+                    item.current =false
+                });
+                data.current=true
+            },
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            }
+        }
     }
 </script>
 
@@ -17,5 +136,42 @@
     height: 100vh;
  background-color:paleturquoise ;
 }
-
+.login-wrap{
+ width: 330px;
+ margin: auto;
+}
+ .menu-tab{
+  text-align: center;
+  li{
+   display: inline-block;
+   width: 88px;
+   line-height: 36px;
+   font-size: 14px;
+   color: #fff;
+   border-radius: 2px;
+  }
+  .current{
+        background-color: green;
+  }
+ }
+.login-form{
+ label{
+  display: block;
+  font-size: 14px;
+  color: blue;
+  text-align: left;
+ }
+}
+ .item-form{
+  margin-bottom: 13px;
+ }
+ .block{
+  display: block;
+  width: 100%;
+ }
+ .center{
+  display: block;
+  text-align: center;
+  margin-left: 80px;
+ }
 </style>
